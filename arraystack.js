8f765149cee68127085f834ec475dce270433696
Christopher Varjas <cvarjas@gmail.com>
2019-03-04 09:43:17 -0500

    Start to implement add and remove methods for ArrayStack

--- arraystack.js
+++ arraystack.js
@@ -4,16 +4,47 @@ class ArrayStack {
 		this.length = 0
 	}
 
+	_indexWithinRange(index) {
+		if (index < 0 || index >= this.length) {
+			throw new Error('IndexError')
+		}
+	}
+
 	get(index) {
-		if (index < 0 || index >= this.length) {throw new Error('IndexError')}
+		this._indexWithinRange(index)
 		return this.array[index]
 	}
 
 	set(index, value) {
-		if (index < 0 || index >= this.length) {throw new Error('IndexError')}
-		let current = this.array[index]
+		this._indexWithinRange(index)
+		let element = this.array[index]
 		this.array[index] = value
-		return current
+		return element
+	}
+
+	add(index, value) {
+		if (index < 0 || index > this.length) {
+			throw new Error('IndexError')
+		}
+		// if (this.length === this.array.length) {resize()}
+		// this.array.splice(index, 0, value)
+		for (let i = this.length; i > index; i--) {
+			this.array[i] = this.array[i-1]
+		}
+		this.array[index] = value
+		this.length ++
+	}
+
+	remove(index) {
+		this._indexWithinRange(index)
+		const element = this.array[index]
+		for (let i = index; i < this.length; i++) {
+			this.array[i] = this.array[i+1]
+		}
+		this.length --
+		// delete this.array[this.length]
+		this.array.length = this.length
+		// if (this.array.length >= 3 * this.length) {resize()}
+		return element
 	}
 }
-
