be8b77af692d33df82702e75ac2816000f4978e3
Christopher Varjas <cvarjas@gmail.com>
2019-03-08 17:16:56 -0500

    Add tests

---
+++ test/arraydeque.test.js
@@ -0,0 +1,23 @@
+const ods = require('../ods.js')
+
+
+// Test ArrayDeque data structure
+test('ArrayDeque add', () => {
+	let a = new ods.ArrayDeque()
+	a.add(0,0)
+	a.add(1,1)
+	a.add(2,2)
+	expect(a.array).toEqual([0,1,2, undefined])
+	expect(a.length).toBe(3)
+})
+
+test('ArrayDeque remove', () => {
+	let a = new ods.ArrayDeque()
+	a.add(0,0)
+	a.add(1,1)
+	a.add(2,2)
+	expect(a.remove(1)).toEqual(1)
+	expect(a.length).toBe(2)
+	expect(a.remove(1)).toEqual(2)
+	expect(a.remove(0)).toEqual(0)
+})
