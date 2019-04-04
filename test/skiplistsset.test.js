2cfb63519a70d4298a5d8d2e106e1cc328bdae44
Christopher Varjas <cvarjas@gmail.com>
2019-04-04 10:26:26 -0400

    Expand tests

--- test/skiplistsset.test.js
+++ test/skiplistsset.test.js
@@ -9,4 +9,22 @@ test('SkiplistSSet add', () => {
 	for (let i = 0; i < 17; i++) {
 		l.add(i)
 	}
+	for (let i = 0; i < 17; i++) {
+		expect(l.add(i)).toBe(false)
+	}
+})
+
+test('SkiplistSSet find', () => {
+	let l = new ods.SkiplistSSet()
+	expect(l.find(0)).toBe(undefined)
+	// Add integer values
+	for (let i = 0; i < 17; i++) {
+		l.add(i)
+	}
+	// Remove integer values
+	for (let i = 0; i < 17; i++) {
+		expect(l.find(i)).toBe(i)
+		// expect(l.length).toBe(16 - i)
+	}
+	expect(l.find(20)).toBe(undefined)
 })
