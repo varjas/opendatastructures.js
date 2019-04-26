eddfb99134a0e77580c6936f22798bf6b756507d
Christopher Varjas <cvarjas@gmail.com>
2019-04-26 09:03:41 -0400

    Start implementation of BinarySearchTree

---
+++ binarysearchtree.js
@@ -0,0 +1,12 @@
+const BinaryTree = require('./binarytree.js')
+
+
+/** Implementation of a binary search tree data structure */
+class BinarySearchTree extends BinaryTree {
+	constructor() {
+		super()
+		this.length = 0
+	}
+}
+
+module.exports = BinarySearchTree
