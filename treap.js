8458ccaebc854c380646bc874fb866ba5816cc47
Christopher Varjas <cvarjas@gmail.com>
2019-05-06 15:17:37 -0400

    Add method

--- treap.js
+++ treap.js
@@ -7,12 +7,34 @@ class Treap extends BinarySearchTree {
 	constructor() {
 		super()
 	}
+
+	rotateLeft(node) {
+		let current = node.right
+		let current.previous = node.previous
+		if (current.previous !== undefined) {
+			if (current.previous.left === node) {
+				current.previous.left = current
+			}else{
+				current.previous.right = current
+			}
+		}
+		node.right = current.left
+		if (node.right !== undefined) {
+			node.right.previous = node
+		}
+		node.previous = current
+		current.left = node
+		if (node === this.root) {
+			this.root = current
+			this.root.previous = undefined
+		}
+	}
 }
 
 class Node extends BaseNode {
 	constructor(value) {
 		super()
-		this.p = Math.random()
+		this.priority = Math.random()
 		this.value = value
 	}
 }
